28f545553998d7009eff9ce293b1c18b4645dc4a
youly <youly001@gmail.com>
2012-10-05 20:21:38 +0800

    fill in layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# encoding: utf-8
 module ApplicationHelper
+	def full_title(page_title)
+		base_title = '教师点评网'
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
 end
